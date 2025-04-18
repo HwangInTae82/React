935c430f61219b8896251eb22afe2392edbe1dad
HwangInTae82 <int0802@naver.com>
2025-04-18 12:42:55 +0900

    2

--- react-project/src/App.jsx
+++ react-project/src/App.jsx
@@ -1,12 +1,16 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import JavaScript from './components/JavaScript'
+import Style from './components/Style'
+import Products from './components/Products'
 
 function App() {
   return (
     <>
-    
+      {/*<JavaScript /> */} 
+      {/*<Style /> */}
+      <Products />
+      
+      
     </>
   )
 }
