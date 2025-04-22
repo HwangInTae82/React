2a4924647731a535a9dee91c2e1ae9a0d708f451
HwangInTae82 <int0802@naver.com>
2025-04-22 09:35:41 +0900

    feat

--- react-project/src/App.jsx
+++ react-project/src/App.jsx
@@ -1,11 +1,14 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import JavaScript from './components/JavaScript'
+import Products from './components/Products'
+import Style from './components/Style'
 
 function App() {
   return (
     <>
+      {/* <JavaScript/> */}
+      {/* <Style /> */}
+      <Products />
     </>
   )
 }
