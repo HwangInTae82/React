f3070c0edc782f72e8f514d28d28723bd0218441
HwangInTae82 <int0802@naver.com>
2025-04-29 12:36:51 +0900

    feat

---
+++ react-project/public/js/async.js
@@ -0,0 +1,69 @@
+const serverDate = [{
+    id: 1,
+    name: "황인태",
+}, {
+    id: 2,
+    name: "이주찬",
+}, {
+    id: 3,
+    name: "전진영",
+}]
+
+// //기존콜백방식
+// function getUser(data, successCallback, errorCallback) {
+//     //일반함수내에서 비동기작업 실행
+//     setTimeout(() => {
+//         //완료 후 callback을 통한 데이터 전달
+//         const user = serverDate.filter(u => u.id === data.id);
+//         if(user.length > 0) successCallback(user);
+//         else errorCallback("user를 찾을 수 없습니다.");
+
+//     }, 3000);
+// }
+
+// getUser({ id: 5 },(user) => {
+//     console.log(user);
+// },(errMsg) => {
+//     console.log(errMsg);
+// })
+
+function getUser(data) {
+    //Promis -> 비동기작업을 실행해줄 객체
+    return new Promise((resolve, reject) => {
+        //resolve : 성공시 실행해줄 함수
+        //reject : 실패시 실행해줄 함수
+
+        //Promise내에서 비동기 함수 실행
+        setTimeout(() => {
+            //완료 후 callback을 통한 데이터 전달
+            const user = serverDate.filter(u => u.id === data.id);
+            if (user.length > 0) resolve(user);
+            else reject("user를 찾을 수 없습니다.");
+
+        }, 3000);
+    })
+}
+
+// getUser({ id: 5 })
+//     .then(result => {
+//         console.log("then 결과 : ", result);
+//     })
+//     .catch(error => {
+//         console.log("error 결과 : ", error);
+//     })
+//     .finally(() => {
+//         console.log("finally 실행");
+//     });
+
+async function testAsync() {
+    try {
+        const result = await getUser({ id: 3 });
+        console.log("await 결과 : ", result);
+    } catch (error) {
+        console.log("catch 결과 : ", error);
+    } finally {
+        console.log("async/await 실행완료");
+    }
+}
+
+testAsync();
